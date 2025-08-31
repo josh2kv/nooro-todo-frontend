732bb3319ebe2527773194d3212e50cb73c27b89
shj <josh2kv@gmail.com>
2025-08-31 20:26:30 +0900

    feat: add empty task state with image and message

--- src/app/(task)/task-list/list.tsx
+++ src/app/(task)/task-list/list.tsx
@@ -1,5 +1,6 @@
 import { Task } from "@/types/task";
 import TaskListItem from "./list-item";
+import Image from "next/image";
 
 interface Props {
   isLoading: boolean;
@@ -9,9 +10,19 @@ interface Props {
 export default function TaskList({ isLoading, error, tasks }: Props) {
   return (
     <div className='space-y-3'>
-      {tasks.map((task) => (
-        <TaskListItem key={task.id} task={task} />
-      ))}
+      {tasks && tasks.length > 0 ? (
+        tasks.map((task) => <TaskListItem key={task.id} task={task} />)
+      ) : (
+        <div className='text-center text-sm text-muted-foreground flex flex-col items-center gap-4 border-t rounded-lg h-[266px] justify-center'>
+          <Image src='/clipboard.png' alt='Empty task' width={56} height={56} />
+          <div className='space-y-3'>
+            <p className='font-bold'>
+              You don't have any tasks registered yet.{" "}
+            </p>
+            <p>Create tasks and organize your to-do items.</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
